eb1b9e1af65b42622b04f4e7c3644b31d1075bd7
yopaseopor <yopaseopor@gmail.com>
2019-04-10 23:46:55 +0200

    Update layerdef.js

--- js_source/layerdef.js
+++ js_source/layerdef.js
@@ -138,9 +138,9 @@ function layerdef(type){
 			),
 			  
             make_layer(
-				QURL + "?data=(way['area:parking:condition'='ticket']['area:parking'='perpendicular'](bbox);node(w););out+skel;",
+				QURL + "?data=(way['area:parking:condition'='free']['area:parking'='perpendicular'](bbox);node(w););out+skel;",
 				name="#dl#cycleway moped=no",
-				defaultDashedLine("blue"),
+				defaultDashedLine("white"),
 				false
 			),
 			  
